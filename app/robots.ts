88935794d8625ac7b2105c652943c8efa7be0c1c
JOAOTIX <joao.software.dev@gmail.com>
2026-09-25 13:17:44 -0500

    feat: add the new name page

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://portfolio-nine-delta-32.vercel.app/sitemap.xml',
+    sitemap: 'https://joao-urteaga-dev.vercel.app/sitemap.xml',
   }
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://portfolio-nine-delta-32.vercel.app'
+  const baseUrl = 'https://joao-urteaga-dev.vercel.app/sitemap.xml'
 
   return [
     {
